5d3dd78a277060def69f8f653f3a883fb8283a37
Azriel <azrihell.l@gmail.com>
2023-03-14 21:18:11 -0500

    while & doWhile loops

--- script2.js
+++ script2.js
@@ -5,8 +5,22 @@ var todo = [
   "study javascript",
   "go to gym",
 ];
-
+/*
 var todoLength = todo.length;
 for (var i=0; i < todo,length; i++) {
   todo.pop();
 }
+
+
+var counterOne = 10;
+while (counterOne > 0) {
+  console.log(counterOne);
+  counterOne--;
+}
+*/
+
+var counterTwo = 10
+do {
+  console.log(counterTwo);
+  counterTwo--;
+} while (counterTwo > 0);
