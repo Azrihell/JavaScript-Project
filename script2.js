f327114e22640fe89ca53fd42a38470d65dd3b51
Azriel <azrihell.l@gmail.com>
2023-03-15 20:26:39 -0500

    todo.forEach

--- script2.js
+++ script2.js
@@ -5,13 +5,18 @@ var todo = [
   "study javascript",
   "go to gym",
 ];
-/*
+
 var todoLength = todo.length;
+
 for (var i=0; i < todo,length; i++) {
   todo.pop();
 }
 
+todo.forEach(function(todo, i) {
+  console.log(todo, i);
+})
 
+/*
 var counterOne = 10;
 while (counterOne > 0) {
   console.log(counterOne);
@@ -19,8 +24,3 @@ while (counterOne > 0) {
 }
 */
 
-var counterTwo = 10
-do {
-  console.log(counterTwo);
-  counterTwo--;
-} while (counterTwo > 0);
