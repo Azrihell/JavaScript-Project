dbe4f877df087f90193ace06c8aa1444969094b8
Azriel <azrihell.l@gmail.com>
2023-03-17 13:00:52 -0500

    many users

--- script.js
+++ script.js
@@ -82,3 +82,35 @@ while (counterOne > 0) {
   counterOne--;
 }
 */
+
+/*var todo = [
+  "clean room",
+  "brush teeth",
+  "exercize",
+  "study javascript",
+  "go to gym",
+];
+
+var todoImportant = [
+  "clean room!",
+  "brush teeth!",
+  "exercize!",
+  "study javascript!",
+  "go to gym!",
+];
+
+var todoLength = todo.length;
+
+for (var i=0; i < todo,length; i++) {
+  console.log(todo[i], i);
+}
+
+function logTodo(todo, i) {
+  console.log(todo, i);
+}
+
+todo.forEach(logTodo);
+todoImportant.forEach(logTodo);
+
+
+*/
--- script2.js
+++ script2.js
@@ -1,32 +1,53 @@
-var todo = [
-  "clean room",
-  "brush teeth",
-  "exercize",
-  "study javascript",
-  "go to gym",
+var database = [
+  {
+    username: "Az",
+    password: "superSecret"
+  },
+  {
+    username: "John",
+    password: "john123"
+  },
+  {
+    username: "Jane",
+    password: "jane123"
+  }
 ];
 
-var todoImportant = [
-  "clean room!",
-  "brush teeth!",
-  "exercize!",
-  "study javascript!",
-  "go to gym!",
+var newsFeed = [
+  {
+    username: "John",
+    timeline: "super tired from all that climbing."
+  },
+  {
+    username: "Jane",
+    timeline: "Climbing is so much fun!"
+  }
 ];
 
-var todoLength = todo.length;
-
-/*
-for (var i=0; i < todo,length; i++) {
-  console.log(todo[i], i);
+function isUserValid(username, password) {
+  for (var i=0; i< database.length; i++) {
+    if(database[i].username === username &&
+        database[i].password === password) {
+          return true
+        }
+    }
+    return false;
 }
-*/
-
-function logTodo(todo, i) {
-  console.log(todo, i);
+function signIn(username, password) {
+  console.log(isUserValid(username, password));
 }
 
-todo.forEach(logTodo);
-todoImportant.forEach(logTodo);
+var userNamePrompt = prompt("What's your username?");
+var passwordPrompt = prompt("What's your password?");
+
+signIn(userNamePrompt, passwordPrompt);
+
+
+
+  //   if (username === database[0].username && password === database[0].password) {
+  //   console.log(newsFeed);
+  // } else {
+  //   alert("Wrong username or password, please try again.");
+  // }
 
 
