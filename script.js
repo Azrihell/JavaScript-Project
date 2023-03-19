d37081a74ed877a112684b6b089951106528bd8f
Azriel <azrihell.l@gmail.com>
2023-03-19 17:51:54 -0500

    mouse events

--- script.js
+++ script.js
@@ -30,23 +30,18 @@ console.log(list);
   }
 };
 
-var list = [
-  {
-    username: "johndoe",
-    password: "secret"
-  },
-  {
-    username: "janedoe",
-    password: "secret123"
-  }
-];
-*/
-
-/*
 var database = [
   {
     username: "Az",
     password: "superSecret"
+  },
+  {
+    username: "John",
+    password: "john123"
+  },
+  {
+    username: "Jane",
+    password: "jane123"
   }
 ];
 
@@ -61,19 +56,30 @@ var newsFeed = [
   }
 ];
 
+function isUserValid(username, password) {
+  for (var i=0; i< database.length; i++) {
+    if(database[i].username === username &&
+        database[i].password === password) {
+          return true
+        }
+    }
+    return false;
+}
+function signIn(username, password) {
+  if (isUserValid(username, password)) {
+    console.log(newsFeed);
+  } else {
+    alert("Wrong username or password, please try again.");
+  }
+
+}
+
 var userNamePrompt = prompt("What's your username?");
 var passwordPrompt = prompt("What's your password?");
 
-function signIn(username, password) {
-  if (username === database[0].username && password === database[0].password) {
-  console.log(newsFeed);
-} else {
-  alert("Wrong username or password, please try again.");
-}
-}
-
 signIn(userNamePrompt, passwordPrompt);
-*/
+
+
 
 /*
 var counterOne = 10;
--- script2.js
+++ script2.js
@@ -1,50 +1,5 @@
-var database = [
-  {
-    username: "Az",
-    password: "superSecret"
-  },
-  {
-    username: "John",
-    password: "john123"
-  },
-  {
-    username: "Jane",
-    password: "jane123"
-  }
-];
+var button = document.getElementsByTagName ("button") [0];
 
-var newsFeed = [
-  {
-    username: "John",
-    timeline: "super tired from all that climbing."
-  },
-  {
-    username: "Jane",
-    timeline: "Climbing is so much fun!"
-  }
-];
-
-function isUserValid(username, password) {
-  for (var i=0; i< database.length; i++) {
-    if(database[i].username === username &&
-        database[i].password === password) {
-          return true
-        }
-    }
-    return false;
-}
-function signIn(username, password) {
-  if (isUserValid(username, password)) {
-    console.log(newsFeed);
-  } else {
-    alert("Wrong username or password, please try again.");
-  }
-
-}
-
-var userNamePrompt = prompt("What's your username?");
-var passwordPrompt = prompt("What's your password?");
-
-signIn(userNamePrompt, passwordPrompt);
-
-
+button.addEventListener("click", function() {
+  console.log("CLICK!");
+})
