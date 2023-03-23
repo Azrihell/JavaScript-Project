4863216774038c8bdfb457ff5ee2c2f1b777ce0c
Azriel <azrihell.l@gmail.com>
2023-03-23 12:01:50 -0500

    cleanup + make color pickers same color as starting background

--- script3.js
+++ script3.js
@@ -10,9 +10,11 @@ function setGradient() {
    + ", " 
    + color2.value 
    + ")";
+
+   css.textContent = body.style.background + ";";
 }
 
-color1.addEventListener("input", setGradient)
+color1.addEventListener("input", setGradient);
 
-color2.addEventListener("input", setGradient)
+color2.addEventListener("input", setGradient);
 
